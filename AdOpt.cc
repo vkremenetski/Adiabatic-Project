02ac574b221801f4e04cf653c0e7fee44e8209bc
Vladimir Kremenetski <jxu0329@barney.cchem.berkeley.edu>
2018-07-16 13:53:17 -0700

    Up to 18 qubits

--- AdOpt.cc
+++ AdOpt.cc
@@ -134,8 +134,11 @@ std::vector<Real> gapAndEntropy(int N, std::vector<int> p, std::vector<Real> w, 
     results.at(1) = entropy;
     return results;
 }
-bool helper(std::vector<Real> v1, std::vector<Real> v2){
+bool helper1(std::vector<Real> v1, std::vector<Real> v2){
     return (v1[0] < v2[0]);
+}
+bool helper2(std::vector<Real> v1, std::vector<Real> v2){
+    return (v1[1] > v2[1]);
 }
 std::vector<Real> minGapAndEntropy(int N, std::vector<int> positions, std::vector<Real> weights, Real step){
     auto results = std::vector<std::vector<Real>>();
@@ -143,8 +146,12 @@ std::vector<Real> minGapAndEntropy(int N, std::vector<int> positions, std::vecto
     for(Real s=0.3+step; s<=0.5; s+= step){
         results.push_back(gapAndEntropy(N, positions, weights, sites, s));
     }
-    std::sort(results.begin(),results.end(), helper);
-    return results[0];
+    std::sort(results.begin(),results.end(), helper1);
+    auto output = std::vector<Real>();
+    output.push_back(results[0][0]);
+    std::sort(results.begin(),results.end(), helper2);
+    output.push_back(results[0][1]);
+    return output;
 }
 void timeToText(string title,int N, std::vector<int> positions, std::vector<Real> weights, Real step){
     ofstream myfile;
@@ -180,6 +187,6 @@ int main(int argc, char* argv[]) {
     Real myweights[] = {3,-4,-4,-2};
     std::vector<int> positions(mypositions,mypositions+4);
     std::vector<Real> weights(myweights,myweights+4);
-    timeToText("EightQubEvo.txt",N,positions,weights,0.01);
-    //qubitCountToText("NQubitEvolution.txt",16,weights,0.02);
+    //timeToText("EightQubEvo.txt",N,positions,weights,0.01);
+    qubitCountToText("NQubitEvolution.txt",18,weights,0.02);
 }
