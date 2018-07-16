de697e55903753c85960a229cfa54764768ce5a1
Vladimir Kremenetski <jxu0329@barney.cchem.berkeley.edu>
2018-07-15 19:11:04 -0700

    Stuff

--- AdOpt.cc
+++ AdOpt.cc
@@ -175,11 +175,11 @@ void qubitCountToText(string title, int UpperQubitNumber, std::vector<Real> weig
 
 
 int main(int argc, char* argv[]) {
-    int N = 7;
+    int N = 8;
     int mypositions[] = {1,N/2+1,N/2,N/2+1};
     Real myweights[] = {3,-4,-4,-2};
     std::vector<int> positions(mypositions,mypositions+4);
     std::vector<Real> weights(myweights,myweights+4);
-    timeToText("SevenQubEvo.txt",N,positions,weights,0.01);
+    timeToText("EightQubEvo.txt",N,positions,weights,0.01);
     //qubitCountToText("NQubitEvolution.txt",16,weights,0.02);
 }
