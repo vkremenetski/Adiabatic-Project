2be88f3bc057a47fc0e41edce6493fcda0d3d461
Vladimir Kremenetski <jxu0329@barney.cchem.berkeley.edu>
2018-07-30 17:05:08 -0700

    More refined

--- AdOpt.cc
+++ AdOpt.cc
@@ -215,7 +215,7 @@ void timeToText(string title,int N, std::vector<int> positions, std::vector<Real
     ofstream myfile;
     myfile.open(title);
     auto sites = SpinHalf(N);
-    for(Real s=0.5; s<=0.8; s+= step){
+    for(Real s=0.65; s<=0.7; s+= step){
         auto results = gapAndEntropy(N, positions, weights, sites, s);
         myfile << s << " " << results.at(0) << " " << results.at(1);
         myfile << "\n";
@@ -249,7 +249,7 @@ int main(int argc, char* argv[]) {
     std::vector<int> positions(mypositions,mypositions+4);
     std::vector<Real> weights(myweights,myweights+4);
     SpinHalf spins = SpinHalf(N);
-    timeToText("SevenQubitEvolution2.txt",N,positions,weights,0.002);
+    timeToText("SevenQubitEvolution2.txt",N,positions,weights,0.001);
     //qubitCountToText("NQubitEvolution.txt",16,weights,0.01);
     /*for(Real s = 0.75; s<= 1; s+=0.01){
         MPO Ham = getHam(N, positions, weights, spins, s);
