92b81b851af5c45e0a4a940a397bac18b5d93ded
Vladimir Kremenetski <jxu0329@barney.cchem.berkeley.edu>
2018-08-07 12:26:10 -0700

    Final check

--- AdOpt.cc
+++ AdOpt.cc
@@ -215,7 +215,7 @@ void timeToText(string title,int N, std::vector<int> positions, std::vector<Real
     ofstream myfile;
     myfile.open(title);
     auto sites = SpinHalf(N);
-    for(Real s=0.5; s<=1; s+= step){
+    for(Real s=0; s<1+step; s+= step){
         auto results = gapAndEntropy(N, positions, weights, sites, s);
         myfile << s << " " << results.at(0) << " " << results.at(1);
         myfile << "\n";
@@ -287,7 +287,7 @@ int main(int argc, char* argv[]) {
     std::vector<Real> weights(myweights,myweights+4);
     SpinHalf spins = SpinHalf(N);
     //overlapToText("Overlap10q.txt", 0.65, 0.75, N, positions, weights, 0.001);
-    timeToText("FiveQubitEvolution2.txt",N,positions,weights,0.005);
+    timeToText("FiveQubitEvolution2.txt",N,positions,weights,0.01);
     //qubitCountToText("NQubitEvolution.txt",16,weights,0.01);
     /*for(Real s = 0.75; s<= 1; s+=0.01){
         MPO Ham = getHam(N, positions, weights, spins, s);
